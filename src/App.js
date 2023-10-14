8156df482814e8907d898717a416d4deae7ca227
davidmoeti8 <129874735+davidmoeti8@users.noreply.github.com>
2023-10-14 10:56:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
         <Route path='/about' element={<About />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
        </Routes>
+       <Home />
        <Footer />
      </Router>
     </div>
